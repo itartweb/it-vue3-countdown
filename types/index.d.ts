acac2d0b90256796bdb73fa6324d23a785b489be
Alex Rozumenko <itartweb@gmail.com>
2023-11-05 15:59:16 +0200

    Added types file

---
+++ types/index.d.ts
@@ -0,0 +1,24 @@
+import {App} from 'vue';
+
+export interface Countdown {
+  deadline?: string;
+  deadlineISO?: string;
+  deadlineDate?: string;
+  countdownSize?: string;
+  labelSize?: string;
+  stop?: boolean;
+  flipAnimation?: boolean;
+  showDays?: boolean;
+  showHours?: boolean;
+  showMinutes?: boolean;
+  showSeconds?: boolean;
+  showLabels?: boolean;
+  labels?: object;
+  mainColor?: string;
+  secondFlipColor?: string;
+  mainFlipBackgroundColor?: string;
+  secondFlipBackgroundColor?: string;
+  labelColor?: string;
+}
+
+export default function install(app: App): void;
